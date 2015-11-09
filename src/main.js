6b8d489faf8b246cc8f3812771f9ac608f42a7dd
jaebradley <jae.b.bradley@gmail.com>
2015-11-09 16:51:35 -0500

    basic todo list is working

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 "use strict";
 
 var Task = React.createClass({
+	handleClick: function() {
+		console.log(this.props.name);
+		console.log(this.props.type);
+		this.props.handleUserSelection(this.props.name, this.props.type);
+	},
 	render: function() {
 		return (
-			<p>{this.props.name}</p>
+		 	<div name={this.props.name} type={this.props.type} onClick={this.handleClick}><p>{this.props.name}</p></div>
 		);
 	}
 });
@@ -12,8 +17,8 @@ var List = React.createClass({
 	render: function() {
 		var taskList = [];
 		this.props.tasks.forEach(function(task) {
-			taskList.push(<Task name={task.name} key={task.name} />);
-		});
+			taskList.push(<Task name={task.name} key={task.name} type={task.type} handleUserSelection={this.props.onUserSelection} />);
+		}.bind(this));
 
 		return (
 			<div>
@@ -24,10 +29,9 @@ var List = React.createClass({
 });
 
 var Todo = React.createClass({
-	render: function() {
+	getInitialState: function() {
 		var incompleteTasks = [];
 		var completeTasks = [];
-		console.log(this.props.allTasks);
 		this.props.allTasks.forEach(function(task) {
 			if (task.type == "complete") {
 				completeTasks.push(task);
@@ -36,16 +40,54 @@ var Todo = React.createClass({
 			}
 		});
 
+		return {
+			incompleteTasks: incompleteTasks,
+			completeTasks: completeTasks
+		};
+	},
+	handleUserSelection: function(taskName, taskType) {
+		var newTaskType;
+		if ("complete" == taskType) {
+			newTaskType = "incomplete";
+		} else {
+			newTaskType = "complete";
+		};
+
+		for (var task in TASKS) {
+			if (taskName == TASKS[task]["name"]) {
+				TASKS[task]["type"] = newTaskType;
+			}
+		}
+
+		var incompleteTasks = [];
+		var completeTasks = [];
+		this.props.allTasks.forEach(function(task) {
+			if (task.type == "complete") {
+				completeTasks.push(task);
+			} else {
+				incompleteTasks.push(task);
+			}
+		});
+
+		console.log(TASKS);
+
+		this.setState({
+			incompleteTasks: incompleteTasks,
+			completeTasks: completeTasks
+		});
+	},
+	render: function() {
+
 		return (
 			<div className="todo">
 				<h1>Todo List</h1>
 				<div className="incompleteTasks">
 					<h2>Incomplete Tasks</h2>
-					<List tasks={incompleteTasks} />
+					<List tasks={this.state.incompleteTasks} onUserSelection={this.handleUserSelection} />
 				</div>
 				<div className="completeTasks">
 					<h2>Complete Tasks</h2>
-					<List tasks={completeTasks} />
+					<List tasks={this.state.completeTasks} onUserSelection={this.handleUserSelection} />
 				</div>
 			</div>
 		);
